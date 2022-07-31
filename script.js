623bc6f553cb69ad6e34bcd80c0d3f7cd7fc0c69
donghyun <ilovekdh1208@ajou.ac.kr>
2022-07-31 13:34:45 +0900

    give animation

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-var logBox = document.querySelector("#loginBox");
+var user_id = document.querySelector(".inputBox").innerText;
+function goMainPage() {
+  window.location.href = "/mainPage/mainPage.html";
+}
+
+gsap.fromTo(
+  "#login",
+  { opacity: 0, x: 300 },
+  { opacity: 1, x: 0, duration: 0.5 }
+);
---
+++ signup/signup.js
@@ -0,0 +1,5 @@
+gsap.fromTo(
+  "#login",
+  { opacity: 0, x: 300 },
+  { opacity: 1, x: 0, duration: 0.5 }
+);
