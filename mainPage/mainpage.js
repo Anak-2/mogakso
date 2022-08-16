856bfc342ad6f2c606b3fff321a6a18eb5b39810
donghyun <ilovekdh1208@ajou.ac.kr>
2022-08-16 14:56:45 +0900

    make site can arrange todos in order time

--- mainPage/mainpage.js
+++ mainPage/mainpage.js
@@ -14,7 +14,6 @@ searchBox.addEventListener("keyup", function (e) {
 
 //delete one list
 function deleteList(e) {
-  // console.dir(e.target);
   let s = e.target.parentElement;
   let t1 = new TimelineMax({
     //remove를 애니메이션 다 끝난 후 호출하기 위한 방법
@@ -65,29 +64,148 @@ function addList(e) {
   if (inputBox.value != "") {
     const listWrapper = document.createElement("div");
     listWrapper.classList.add("list-wrapper");
-    list.appendChild(listWrapper);
     const listItem = document.createElement("li");
+
+    //combine list with inputdate and put in listWrapper
+    const inputTime = document.querySelector(".time-input");
+    const deadline = document.createElement("span");
+    deadline.innerText = inputTime.value;
+    deadline.classList.add("deadline");
     listItem.classList.add("list-item");
     listItem.innerText = inputBox.value;
+    listItem.appendChild(deadline);
     listWrapper.appendChild(listItem);
+
+    // make check Btn and put in listWrapper
     const checkBtn = document.createElement("button");
     checkBtn.addEventListener("click", checkList);
     checkBtn.classList.add("complete-check");
     checkBtn.innerText = "✅";
     listWrapper.appendChild(checkBtn);
+
+    //make delete Btn and put in listWrapper
     const deleteBtn = document.createElement("button");
     deleteBtn.addEventListener("click", deleteList);
     deleteBtn.classList.add("delete-btn");
     deleteBtn.innerText = "❌";
     listWrapper.appendChild(deleteBtn);
+
+    // input in date order
+    const inputDate = document.querySelector(".date-input");
+    const dateItem = document.querySelectorAll(".date-item");
+    let outerListIndex = 0;
+    if (dateItem.length == 0) {
+      const outerList = document.createElement("div");
+      outerList.classList.add("outer-list");
+      outerList.appendChild(dateItem);
+      outerList.appendChild(listWrapper);
+      list.appendChild(outerList);
+    } else {
+      outerListIndex = findDateLocation(inputDate, dateItem);
+    }
+    const outerListArr = document.querySelectorAll(".outer-list");
+
+    // input in time order
+    // finally, find index where to put todo
+    const listWrapperIndex = findTimeLocation(
+      outerListArr[outerListIndex],
+      inputTime
+    );
+    outerListArr[outerListIndex].children[
+      listWrapperIndex
+    ].insertAdjacentElement("afterend", listWrapper);
+    //init input values and date input
     inputBox.value = "";
+    inputDate.value = inputDate.min;
+    // init time input
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    inputTime.value = `${hours}:${minutes}`;
+  }
+}
+
+// decide where to put the date and return the index which points to a place
+// to write list-wrapper
+function findDateLocation(inputDate, dateItem) {
+  const inputDateArr = inputDate.value.split("-");
+  const newDate = document.createElement("div");
+  newDate.classList.add("date-item");
+  newDate.innerText = inputDate.value;
+  const outerList = document.createElement("div");
+  outerList.classList.add("outer-list");
+  outerList.appendChild(newDate);
+  for (let i = 0; i < dateItem.length; i++) {
+    const date = dateItem[i].innerText.split("-");
+    if (inputDateArr[0] == date[0]) {
+      if (inputDateArr[1] == date[1]) {
+        if (inputDateArr[2] == date[2]) {
+          return i;
+        } else {
+          if (inputDateArr[2] > date[2] && i == dateItem.length - 1) {
+            dateItem[i].parentElement.insertAdjacentElement(
+              "afterend",
+              outerList
+            );
+            return i + 1;
+          } else if (inputDateArr[2] < date[2]) {
+            dateItem[i].parentElement.insertAdjacentElement(
+              "beforebegin",
+              outerList
+            );
+            return i;
+          }
+        }
+      } else if (inputDateArr[1] > date[1] && i == dateItem.length - 1) {
+        dateItem[i].parentElement.insertAdjacentElement("afterend", outerList);
+        return i + 1;
+      } else if (inputDateArr[1] < date[1]) {
+        dateItem[i].parentElement.insertAdjacentElement(
+          "beforebegin",
+          outerList
+        );
+        return i;
+      }
+    } else if (inputDateArr[0] > date[0] && i == dateItem.length - 1) {
+      dateItem[i].parentElement.insertAdjacentElement("afterend", outerList);
+      return i + 1;
+    } else if (inputDateArr[0] < date[0]) {
+      dateItem[i].parentElement.insertAdjacentElement("beforebegin", outerList);
+      return i;
+    }
+  }
+}
+
+// decide where to put the time and return the index which points to a place
+// to write to-do
+function findTimeLocation(wrapperArr, inputTime) {
+  const wrapperArrLen = wrapperArr.children.length;
+  const inputTimeArr = inputTime.value.split(":");
+  if (wrapperArrLen == 1) {
+    return 0;
+  }
+  for (let i = 1; i < wrapperArrLen; i++) {
+    const time =
+      wrapperArr.children[i].children[0].firstElementChild.innerText.split(":");
+    if (time[0] == inputTimeArr[0]) {
+      if (time[1] == inputTimeArr[1] && i == wrapperArrLen - 1) {
+        return i;
+      } else if (time[1] < inputTimeArr[1] && i == wrapperArrLen - 1) {
+        return i;
+      } else if (time[1] > inputTimeArr[1]) {
+        return i - 1;
+      }
+    } else if (time[0] < inputTimeArr[0] && i == wrapperArrLen - 1) {
+      return i + 1;
+    } else if (time[0] > inputTimeArr[0]) {
+      return i - 1;
+    }
   }
 }
 
 function checkList(e) {
   let t = e.target.parentElement;
   let t2 = e.target.previousElementSibling;
-  console.dir(t2);
   if (t2.classList.contains("complete-list")) {
     t2.classList.remove("complete-list");
     gsap.to(t, { duration: 1, opacity: 1, ease: "line" });
@@ -111,7 +229,6 @@ for (let i = 0; i < checkBtn.length; i++) {
 
 //open side menu
 function openSideMenu() {
-  console.log("hi...");
   const sideMenu = document.querySelector(".side-menu");
   if (sideMenu.classList.contains("side-menu-hidden")) {
     sideMenu.classList.remove("side-menu-hidden");
@@ -175,6 +292,7 @@ function getClock() {
   let dayZero = String(day).padStart(2, 0);
   const inputDate = document.querySelector(".date-input");
   inputDate.value = `${year}-${monthZero}-${dayZero}`;
+  inputDate.min = `${year}-${monthZero}-${dayZero}`;
 }
 
 getClock();
